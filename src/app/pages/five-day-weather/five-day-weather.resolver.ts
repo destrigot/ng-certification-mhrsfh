0072677bc2bceab0a506818dec362c1bc7145cd8
destrigot <juan.tc.dai@gmail.com>
2021-07-09 09:08:50 +0200

    2 files modified

--- src/app/pages/five-day-weather/five-day-weather.resolver.ts
+++ src/app/pages/five-day-weather/five-day-weather.resolver.ts
@@ -1,17 +1,22 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
+import { ActivatedRouteSnapshot, Resolve, Router } from "@angular/router";
 import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { OpenWeatherService } from "../../services/open-weather.service";
 
 @Injectable()
 export class FiveDayWeatherResolver implements Resolve<any> {
   constructor(
+    private router: Router,
     private openWeatherService: OpenWeatherService
   ) {}
 
   public resolve (activatedRoute: ActivatedRouteSnapshot): Observable<any> {
     const zipcode: string = activatedRoute.paramMap.get('zipcode');
 
-    return this.openWeatherService.getFiveDayWeather(zipcode);
+    return this.openWeatherService.getFiveDayWeather(zipcode)
+      .pipe(
+        catchError(() => this.router.navigate(['']))
+      );
   }
 }
